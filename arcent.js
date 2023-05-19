a73331cf4e1ed583d5ebf4cf2eb828f5e0a53b8a
Kirill Stahl <13516176@student.uts.edu.au>
2023-05-19 20:47:54 +1000

    Initial upload

---
+++ arcent.js
@@ -0,0 +1,209 @@
+import * as THREE from 'three';
+import {GUI} from './build/dat.gui.module.js';
+import {OrbitControls} from './build/OrbitControls.js';
+import {initSierpinski} from './sierpinski.js';
+
+//==================================
+//=======The First Arcent==========
+//==================================
+
+var renderer, scene, camera; 
+var mesh, animation;
+
+let arcentMaterial, uniforms;
+let clock = new THREE.Clock();
+
+const MAX_TIME = 5.0;
+
+const options = {
+    time: 0,
+    speed: 0.01,
+    animate: true,
+    forward: true,
+    loop: true
+};
+
+renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.sortObjects = false;
+document.body.appendChild(renderer.domElement);
+
+scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const clearScene = function () {
+    scene.children.forEach(c => {
+        c.geometry.dispose();
+    });
+    scene.children = [];
+};
+
+/**
+ * Sets the timestamp of the animation. This will generate a new geometry.
+ */
+const setTime = function (time) {
+    clearScene();
+    if (!time) time = 0;
+
+    time = Math.min(MAX_TIME, Math.max(0, time));
+    options.time = time;
+
+    mesh = new THREE.Mesh(initSierpinski.generate(time), arcentMaterial);
+    mesh.position.set(0, -0.5, 0);
+    mesh.scale.setScalar(2);
+    scene.add(mesh);
+};
+
+uniforms = { 
+    u_resolution:
+    {
+        type: "v2",
+        value : new THREE.Vector2(window.innerWidth, window.innerHeight)
+    },
+    u_fractalIndex:
+    {
+        type: "f",
+        value: 10.0
+    },
+    u_time:
+    {
+        type: "f",
+        value: 0.0
+    },
+    u_color:
+    {
+        type: "f",
+        value: 1.01
+    }
+}
+
+arcentMaterial = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    uniforms: uniforms,
+    side: THREE.DoubleSide
+});
+
+const gui = new GUI();
+gui.add(uniforms.u_fractalIndex, "value", 0, 100.0, 0.001).name("Arcent Corruption");
+gui.add(uniforms.u_color, "value", 0, 10.0, 0.001).name("Enropy Purity");
+
+let lastTime = 0;
+const animate = function(){
+    const now = performance.now();
+    if (!lastTime) lastTime = now;
+    
+    // calculate the new timestamp using dt.
+    // it stops at MAX_TIME / 0, unless options.loop === true
+    if (options.animate) 
+    {
+        if (animation) {
+            window.cancelAnimationFrame(animation);
+        } 
+
+        const dt = now - lastTime;
+        let newTime = 0;
+
+        if (options.forward) {
+            
+            newTime = options.time + options.speed;
+            while (newTime >= MAX_TIME) 
+            {
+                if (options.loop) 
+                {
+                    newTime -= MAX_TIME;             
+                    if(Math.round(newTime) == 0 && options.forward)
+                    {
+                        newTime = MAX_TIME - 0.1; 
+                        options.forward = false;
+                    }                                                                                                     
+                } 
+                else 
+                {        
+                    newTime = MAX_TIME;                     
+                }           
+            }                     
+        } 
+        else 
+        {  
+            newTime = options.time - options.speed;
+            while (newTime <= 0.0) 
+            {
+                if (options.loop) 
+                {                          
+                    newTime += MAX_TIME;
+                    if(Math.round(newTime) == MAX_TIME && !options.forward)
+                    {
+                        newTime = 0.1;
+                        options.forward = true; 
+                    }        
+                } 
+                else 
+                {                   
+                    newTime = 0;         
+                }                      
+            }    
+        }
+ 
+        if (options.time !== newTime) {
+            setTime(newTime);
+        }   
+    }
+
+    let increment = 0.03;
+    scene.rotation.x += increment;
+    scene.rotation.y += increment;
+    scene.rotation.z += increment;
+
+    controls.update();
+    animateShader();
+    renderer.render(scene, camera);
+    animation = window.requestAnimationFrame(animate)
+}
+
+const animateShader = function(){
+
+    const deltaTime = clock.getDelta();
+    uniforms.u_time.value += deltaTime * 20;
+}
+const resetCamera = function () {
+    camera.position.set(1.5, 1, 1.5);
+    camera.lookAt(0,0,0);
+};
+
+window.addEventListener('resize', onWindowResize());
+function onWindowResize(event){
+    resetCamera();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    uniforms.u_resolution.value.x = renderer.domElement.width;
+    uniforms.u_resolution.value.y = renderer.domElement.height;
+};
+
+window.addEventListener('click', () => {PlayAudio()}, {once: true});
+function PlayAudio(){
+    //Music
+    const listener = new THREE.AudioListener();
+    camera.add(listener);
+
+    const audioLoader = new THREE.AudioLoader();
+    const backgroundMusic = new THREE.Audio(listener);
+
+    audioLoader.load("./music/itiscoming.mp3", function(buffer)
+    {
+        backgroundMusic.setBuffer(buffer);
+        backgroundMusic.setLoop(true);
+        backgroundMusic.setPlaybackRate(0.5);
+        backgroundMusic.setVolume(0.5);
+        backgroundMusic.play();
+    });
+}
+// initial camera position
+resetCamera();
+
+// trigger render loop
+animate();
---
+++ sierpinski.js
@@ -0,0 +1,233 @@
+import * as THREE from 'three';
+
+const initSierpinski = function () {
+
+    /**
+     * Generates the vertices of the base triangle, already split into four triangles representing the sides of the
+     * tetrahedron.
+     */
+    const createBaseVertices = function () {
+        const vecA = new THREE.Vector3().setFromCylindrical(new THREE.Cylindrical(1, 0));
+        const vecB = new THREE.Vector3().setFromCylindrical(new THREE.Cylindrical(1, 2 * Math.PI / 3));
+        const vecC = new THREE.Vector3().setFromCylindrical(new THREE.Cylindrical(1, 4 * Math.PI / 3));
+
+        const vecAB = vecA.clone().add(vecB).divideScalar(2);
+        const vecBC = vecB.clone().add(vecC).divideScalar(2);
+        const vecCA = vecC.clone().add(vecA).divideScalar(2);
+
+        return {
+            vertices: [
+                vecA, vecCA, vecAB,
+                vecB, vecAB.clone(), vecBC,
+                vecC, vecBC.clone(), vecCA.clone(),
+                vecAB.clone(), vecCA.clone(), vecBC.clone()
+            ],
+            colors: [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]]
+        };
+    };
+
+    /**
+     * Folds the base triangle into a tetrahedron.
+     */
+    const foldBasicVerticesUp = function (vertices, percentage) {
+        for (let i = 0; i < vertices.length; i = i + 3) {
+            if ((i / 3) % 4 === 3) {
+                // skip the bottom
+                continue;
+            }
+
+            const vecA = vertices[i];
+            const vecB = vertices[i + 1];
+            const vecC = vertices[i + 2];
+
+            const vecBC = vecB.clone().add(vecC).divideScalar(2);
+            const axis = vecC.clone().sub(vecB).normalize();
+
+            const vecABC = vecA.clone().sub(vecBC);
+            vecABC.applyAxisAngle(axis, -percentage * (Math.PI - Math.acos(1 / 3)));
+
+            vecA.copy(vecBC).add(vecABC);
+        }
+    };
+
+    /**
+     * Splits all four sides of the tetrahedron into 6 triangles. The colors of the inner 3, which will be folded into
+     * the inside, are interpolated between the color of the origin side and the color of the destination.
+     */
+    const split = function (vertices, colors, fraction) {
+        const newVertices = [];
+        const newColors = [];
+
+        for (let i = 0; i < vertices.length; i = i + 3) {
+            const vecA = vertices[i];
+            const vecB = vertices[i + 1];
+            const vecC = vertices[i + 2];
+
+            const vecAB = vecA.clone().add(vecB).divideScalar(2);
+            const vecBC = vecB.clone().add(vecC).divideScalar(2);
+            const vecCA = vecC.clone().add(vecA).divideScalar(2);
+            const vecABC = vecA.clone().add(vecB).add(vecC).divideScalar(3);
+
+            newVertices.push(
+                vecA.clone(), vecAB.clone(), vecCA.clone(),
+                vecAB.clone(), vecB.clone(), vecBC.clone(),
+                vecCA.clone(), vecBC.clone(), vecC.clone(),
+                vecABC.clone(), vecCA.clone(), vecAB.clone(),
+                vecABC.clone(), vecAB.clone(), vecBC.clone(),
+                vecABC.clone(), vecBC.clone(), vecCA.clone()
+            );
+
+            const c0 = colors[i / 3];
+            let c1, c2, c3;
+            if (c0[0] === 1) {
+                c1 = [1 - fraction, 0, 0]; // red => black
+                c2 = [1 - fraction, fraction, 0]; // red => green
+                c3 = [1 - fraction, 0, fraction]; // red => blue
+            } else if (c0[1] === 1) {
+                c1 = [0, 1 - fraction, 0]; // green => black
+                c2 = [0, 1 - fraction, fraction]; // green => blue
+                c3 = [fraction, 1 - fraction, 0]; // green => red
+            } else if (c0[2] === 1) {
+                c1 = [0, 0, 1 - fraction]; // blue => black
+                c2 = [fraction, 0, 1 - fraction]; // blue => red
+                c3 = [0, fraction, 1 - fraction]; // blue => green
+            } else {
+                c1 = [0, 0, fraction]; // black => blue
+                c2 = [0, fraction, 0]; // black => green
+                c3 = [fraction, 0, 0]; // black => red
+            }
+
+            newColors.push(c0, c0, c0, c1, c2, c3);
+        }
+        return {vertices: newVertices, colors: newColors};
+    };
+
+    /**
+     * Folds in a single triangle.
+     */
+    const foldInTriangle = function (vertices, i0, i1, i2, percentage) {
+        const vecA = vertices[i0];
+        const vecB = vertices[i1];
+        const vecC = vertices[i2];
+
+        const vecBC = vecB.clone().add(vecC).divideScalar(2);
+        const axis = vecB.clone().sub(vecC).normalize();
+
+        const vecABC = vecA.clone().sub(vecBC);
+        vecABC.applyAxisAngle(axis, percentage * (Math.PI - Math.acos(1 / 3)));
+
+        vecA.copy(vecBC).add(vecABC);
+    };
+
+    /**
+     * Folds in the three inner triangles on all sides.
+     */
+    const foldIn = function (vertices, percentage) {
+        for (let i = 0; i < vertices.length; i = i + 3 * 6) {
+            foldInTriangle(vertices, i + 9, i + 10, i + 11, percentage);
+            foldInTriangle(vertices, i + 12, i + 13, i + 14, percentage);
+            foldInTriangle(vertices, i + 15, i + 16, i + 17, percentage);
+        }
+    };
+
+    /**
+     * Recursively calculates the positions of the sub-tetrahedrons.
+     */
+    const calculateSubPositions = function (depth, bases = [new THREE.Vector3()]) {
+        if (depth < 1) {
+            return bases;
+        }
+
+        const height = 0.7071067811865477;
+        const fraction = 1 / Math.pow(2, depth);
+        const newBases = [];
+        bases.forEach(base => {
+            newBases.push(
+                new THREE.Vector3(0, height * fraction, 0).add(base),
+                new THREE.Vector3().setFromCylindrical(new THREE.Cylindrical(0.5 * fraction, Math.PI / 3)).add(base),
+                new THREE.Vector3().setFromCylindrical(new THREE.Cylindrical(0.5 * fraction, 3 * Math.PI / 3)).add(base),
+                new THREE.Vector3().setFromCylindrical(new THREE.Cylindrical(0.5 * fraction, 5 * Math.PI / 3)).add(base)
+            );
+        });
+
+        return depth > 1 ? calculateSubPositions(depth - 1, newBases) : newBases;
+    };
+
+    const calculateNormals = function (vertices) {
+        const normals = new Array(vertices.length / 3);
+        for (let i = 0; i < vertices.length; i = i + 3) {
+            const vecA = vertices[i];
+            const vecB = vertices[i + 1];
+            const vecC = vertices[i + 2];
+
+            normals[i/3] = vecA.clone().sub(vecB).normalize().cross(vecA.clone().sub(vecC).normalize()).normalize();
+        }
+        return normals;
+    };
+
+    // this caches precalculated positions of the sub-tetrahedrons
+    const positionsCache = {};
+
+    return {
+        /**
+         * Generates the geometry of the sierpinski tetrahedron.
+         */
+        generate: function (time) {
+            // generate the base triangle and fold it up to a tetrahedron
+            let {vertices, colors} = createBaseVertices();
+            foldBasicVerticesUp(vertices, Math.min(1, time));
+
+            // calulate the positions of all sub-tetrahedrons
+            const depth = Math.floor(time) - 1;
+            let positions = positionsCache[depth];
+            if (!positions) {
+                positions = positionsCache[depth] = calculateSubPositions(depth);
+            }
+
+            // time > 1 means, we need to fold in the sides of the tetrahedron
+            if (time > 1) {
+                if (time % 1 !== 0) {
+                    ({vertices, colors} = split(vertices, colors, time % 1));
+                    foldIn(vertices, time % 1);
+                }
+
+                vertices.forEach(v => {
+                    v.divideScalar(Math.pow(2, Math.floor(time) - 1));
+                });
+            }
+
+            // calculate the normals
+            const normals = calculateNormals(vertices);
+
+            // repeat the geometry at every position and store it into attribute buffer
+            const bufferVertices = new Array(positions.length * vertices.length * 3);
+            const bufferColors = new Array(positions.length * vertices.length * 3);
+            const bufferNormals = new Array(positions.length * vertices.length * 3);
+            positions.forEach((p, i) => {
+                vertices.forEach((v, j) => {
+                    const offset = (i * vertices.length + j) * 3;
+                    bufferVertices[offset] = v.x + p.x;
+                    bufferVertices[offset + 1] = v.y + p.y;
+                    bufferVertices[offset + 2] = v.z + p.z;
+
+                    bufferNormals[offset] = normals[Math.floor(j / 3)].x;
+                    bufferNormals[offset + 1] = normals[Math.floor(j / 3)].y;
+                    bufferNormals[offset + 2] = normals[Math.floor(j / 3)].z;
+
+                    bufferColors[offset] = colors[Math.floor(j / 3)][0];
+                    bufferColors[offset + 1] = colors[Math.floor(j / 3)][1];
+                    bufferColors[offset + 2] = colors[Math.floor(j / 3)][2];
+                });
+            });
+
+            const geometry = new THREE.BufferGeometry();
+            geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(bufferVertices), 3));
+            geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(bufferColors), 3));
+            geometry.setAttribute('cnormal', new THREE.BufferAttribute(new Float32Array(bufferNormals), 3));
+
+            return geometry;
+        }
+    };
+}();
+
+export { initSierpinski };
